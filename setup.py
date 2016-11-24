e62e69bbaf9e1a4df0d5b5ec505122682ab5f962
Jerome Leclanche <jerome@leclan.ch>
2016-11-24 19:39:17 +0200

    Release 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-__version__ = "1.0"
+__version__ = "1.1"
 __author__ = "Jerome Leclanche"
 __email__ = "jerome@leclan.ch"
 
