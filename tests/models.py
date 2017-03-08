e07f19bb7629fe16eb9e106fc59fbc95bf319d7d
Jerome Leclanche <jerome@leclan.ch>
2017-03-08 11:32:30 +0200

    tests: Rename TestModel/TestEnum to avoid using a Test* prefix

--- tests/models.py
+++ tests/models.py
@@ -3,13 +3,13 @@ from django.db import models
 from django_intenum import IntEnumField
 
 
-class TestEnum(IntEnum):
+class SampleEnum(IntEnum):
 	A = 1
 	B = 2
 	C = 3
 	D = 99
 
 
-class TestModel(models.Model):
-	int_field = IntEnumField(enum=TestEnum)
-	null_field = IntEnumField(enum=TestEnum, null=True)
+class SampleModel(models.Model):
+	int_field = IntEnumField(enum=SampleEnum)
+	null_field = IntEnumField(enum=SampleEnum, null=True)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,21 @@
 import pytest
 from enum import IntEnum
-from .models import TestEnum, TestModel
+from .models import SampleEnum, SampleModel
 
 
 @pytest.mark.django_db
 def test_field_value():
-	m = TestModel(int_field=TestEnum.A)
+	m = SampleModel(int_field=SampleEnum.A)
 	m.save()
-	assert m.int_field == TestEnum.A
+	assert m.int_field == SampleEnum.A
 	assert m.int_field == 1
 	assert isinstance(m.int_field, IntEnum)
 	assert m.null_field is None
 
-	m.null_field = TestEnum.B
+	m.null_field = SampleEnum.B
 	m.save()
 	assert m.null_field == 2
-	assert m.null_field == TestEnum.B
+	assert m.null_field == SampleEnum.B
 	assert isinstance(m.null_field, IntEnum)
 
 	m.int_field = 1000
