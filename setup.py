6e3e0a3ddbfb15281badd81229d6404e19eae21f
Jerome Leclanche <jerome@leclan.ch>
2017-03-08 11:51:11 +0200

    Move most setup configuration to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.command.test import test as TestCommand
-
-
-__version__ = "1.1"
-__author__ = "Jerome Leclanche"
-__email__ = "jerome@leclan.ch"
 
 
 class PyTestCommand(TestCommand):
@@ -20,43 +15,12 @@ class PyTestCommand(TestCommand):
 		exit(pytest.main(test_args))
 
 
-CLASSIFIERS = [
-	"Development Status :: 5 - Production/Stable",
-	"Environment :: Web Environment",
-	"Framework :: Django",
-	"Intended Audience :: Developers",
-	"License :: OSI Approved :: MIT License",
-	"Programming Language :: Python",
-	"Programming Language :: Python :: 2.7",
-	"Programming Language :: Python :: 3",
-	"Programming Language :: Python :: 3.4",
-	"Programming Language :: Python :: 3.5",
-	"Topic :: Games/Entertainment :: Simulation",
-]
-
-requirements = [
-	"Django >= 1.10",
-]
-
 test_requirements = [
 	"pytest",
 	"pytest-django",
 ]
 
 setup(
-	name="django-intenumfield",
-	version=__version__,
-	packages=find_packages(),
-	author=__author__,
-	author_email=__email__,
-	maintainer=__author__,
-	maintainer_email=__email__,
-	url="https://github.com/HearthSim/django-intenumfield",
-	description="An IntEnumField for Django",
-	download_url="https://github.com/HearthSim/django-intenumfield/tarball/master",
-	classifiers=CLASSIFIERS,
-	license="MIT",
-	install_requires=requirements,
 	tests_require=test_requirements,
 	cmdclass={"test": PyTestCommand},
 )
