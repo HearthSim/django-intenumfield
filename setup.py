efcb8d6122e35f2fc711d7d1e237cb36afff5339
Jerome Leclanche <jerome@leclan.ch>
2017-03-08 11:30:53 +0200

    Use tox to run the tests on multiple environments

--- setup.py
+++ setup.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 
-import os
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 
-class PyTestCommand(TestCommand):
-	def run_tests(self):
-		import django
-		import pytest
-		os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
-		django.setup()
-		test_args = []
-		exit(pytest.main(test_args))
-
-
-test_requirements = [
-	"pytest",
-	"pytest-django",
-]
-
-setup(
-	tests_require=test_requirements,
-	cmdclass={"test": PyTestCommand},
-)
+setup()
